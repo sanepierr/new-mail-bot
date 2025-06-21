fdd8a85a6db041f34146eff12fba128bbea9c7ab
Mokili Promise Pierre <132935466+sanepierr@users.noreply.github.com>
2025-06-21 16:11:51 +0300

    edited configurations

---
+++ utils.py
@@ -0,0 +1,29 @@
+import feedparser
+
+FEEDS = {
+    "TechCrunch": "https://techcrunch.com/feed/",
+    "The Verge": "https://www.theverge.com/rss/index.xml",
+    "CNET": "https://www.cnet.com/rss/news/",
+    "Engadget": "https://www.engadget.com/rss.xml",
+    "Ars Technica": "https://feeds.arstechnica.com/arstechnica/index"
+}
+
+def get_headlines(limit=3):
+    headlines = []
+    for name, url in FEEDS.items():
+        feed = feedparser.parse(url)
+        entries = feed.entries[:limit]
+        for entry in entries:
+            headlines.append({
+                "source": name,
+                "title": entry.title,
+                "link": entry.link
+            })
+    return headlines
+
+def format_email(headlines):
+    body = "📰 <b>Latest Tech News Digest</b><br><br>"
+    for item in headlines:
+        body += f"<b>{item['source']}</b>: <a href='{item['link']}'>{item['title']}</a><br><br>"
+    body += "<br>--<br><i>Sent by Tech News Bot</i>"
+    return body
