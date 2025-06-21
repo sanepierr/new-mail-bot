8d12a27912f1ea75d1db6450de0b7f43c6e4e81e
Mokili Promise Pierre <132935466+sanepierr@users.noreply.github.com>
2025-06-21 15:21:38 +0300

    configured SendGridAPIClient

--- main.py
+++ main.py
@@ -1,11 +1,32 @@
-from bs4 import BeautifulSoup
-import requests
+import os
+from sendgrid import SendGridAPIClient
+from sendgrid.helpers.mail import Mail
+from utils import get_headlines, format_email
+from dotenv import load_dotenv
 
-url = requests.get('https://techcrunch.com/feed/')
-soup = BeautifulSoup(url.content, 'xml')
-news = soup.find('item')
-title =  news.title.text
-link = news.link.text
-publication_date = news.pubDate.text
-description = news.description.text
-print(description)
+load_dotenv()
+
+def send_email():
+    api_key = os.getenv("SENDGRID_API_KEY")
+    sender = os.getenv("SENDER_EMAIL")
+    recipients = os.getenv("RECIPIENT_EMAILS").split(',')
+
+    headlines = get_headlines()
+    content = format_email(headlines)
+
+    message = Mail(
+        from_email=sender,
+        to_emails=recipients,
+        subject="🗞️ Your Daily Tech Digest",
+        html_content=content
+    )
+
+    try:
+        sg = SendGridAPIClient(api_key)
+        response = sg.send(message)
+        print("Email sent:", response.status_code)
+    except Exception as e:
+        print("Error sending email:", str(e))
+
+if __name__ == "__main__":
+    send_email()
